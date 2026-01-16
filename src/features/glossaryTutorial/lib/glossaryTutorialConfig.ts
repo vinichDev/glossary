5271988a8b5c3faa50cd3b5438f90bb91375a1fd
Aleksandr Vinichenko <vinich.yota@gmail.com>
2026-01-16 04:18:42 +0300

    Prepare app for A/B testing

--- src/features/glossaryTutorial/lib/glossaryTutorialConfig.ts
+++ src/features/glossaryTutorial/lib/glossaryTutorialConfig.ts
@@ -1,3 +1,23 @@
 export const GLOSSARY_TUTORIAL_ENABLED = true;
 
 export const GLOSSARY_TUTORIAL_STORAGE_KEY = "glossaryTutorialCompleted";
+
+export const GLOSSARY_TUTORIAL_VARIANT_STORAGE_KEY =
+  "glossaryTutorialVariant";
+
+export const GLOSSARY_TUTORIAL_VARIANTS = {
+  withTutorial: "withTutorial",
+  withoutTutorial: "withoutTutorial"
+} as const;
+
+const metrikaCounterId = Number(process.env.NEXT_PUBLIC_METRIKA_ID);
+
+export const GLOSSARY_TUTORIAL_METRIKA_COUNTER_ID =
+  Number.isFinite(metrikaCounterId) && metrikaCounterId > 0
+    ? metrikaCounterId
+    : null;
+
+export const GLOSSARY_TUTORIAL_METRIKA_GOALS = {
+  withTutorial: "glossary_card_open_with_tutorial",
+  withoutTutorial: "glossary_card_open_without_tutorial"
+} as const;
---
+++ src/shared/ui/YandexMetrika/YandexMetrika.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import Script from "next/script";
+
+type YandexMetrikaProps = {
+  counterId?: string;
+};
+
+export const YandexMetrika = ({ counterId }: YandexMetrikaProps) => {
+  if (!counterId) {
+    return null;
+  }
+
+  return (
+    <>
+      <Script
+        id="yandex-metrika"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");ym(${counterId}, "init", { clickmap: true, trackLinks: true, accurateTrackBounce: true });`
+        }}
+      />
+      <noscript>
+        <div>
+          <img
+            src={`https://mc.yandex.ru/watch/${counterId}`}
+            style={{ position: "absolute", left: "-9999px" }}
+            alt=""
+          />
+        </div>
+      </noscript>
+    </>
+  );
+};
